c279ca7ea3f0726b5e767d0e95acb264db56f351
Myers Carpenter <myers@maski.org>
2021-01-26 12:40:36 -0500

    test for first requerement, currently failing

--- jobtrack/tests.py
+++ jobtrack/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+
+class JobTrackAPITests(TestCase):
+    def test_submit_job(self):
+        body = {"body": "cheese gromit!"}
+        response = self.client.post(
+            "/request", json.dumps(body), content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
