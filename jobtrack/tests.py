bf474cc6f902c21e1105d75b5a41766978bafd2d
Myers Carpenter <myers@maski.org>
2021-01-26 12:50:14 -0500

    make first test check data model is being updated

--- jobtrack/tests.py
+++ jobtrack/tests.py
@@ -3,10 +3,14 @@ import json
 from django.test import TestCase
 from django.test import Client
 
+from .models import Job
+
 class JobTrackAPITests(TestCase):
     def test_submit_job(self):
+        self.assertEqual(0, Job.objects.count())
         body = {"body": "cheese gromit!"}
         response = self.client.post(
             "/request", json.dumps(body), content_type="application/json"
         )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(1, Job.objects.count())
